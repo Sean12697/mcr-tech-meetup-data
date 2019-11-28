60914a8adc2206aa11a6fc2065d71030b814bb62
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-11-28 12:40:25 +0000

    Updated Readme & Generated Content

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# mcr-tech-meetup-data
+# Manchester Tech Meetup Data (CompiledMCR Data)
  
+Using the [Meetup API](https://www.meetup.com/meetup_api/) we have been able to gather raw data from the technology Meetup scene in Manchester. The purpose of curating this data is to assist the community by producing data analytics upon it, and also performing interesting data visualizations.
+
+## Running 
+
+If you wish to generate fresh data yourself, which will be placed in the [_data](_data) folder, you can run the following command through your command prompt (assuming you have previously ran `npm i` with NPM installed).
+
+```
+npm run pull
+```
+
+We do plan to include web pages showing off interesting data visualizations, which will be generated in a `_site` folder, also including an index page. To generate these pages you can run the following command:
+
+```
+npm run gen
+```
--- script.js
+++ script.js
@@ -1,12 +1,78 @@
 module.exports = (groups, events, attendees) => {
-    const fs = require("fs"), shell = require("shelljs"), copydir = require("copy-dir"), beautify = require('beautify'), beaut = (obj) => beautify(JSON.stringify(obj), { format: 'json' });
+    const fs = require("fs"), 
+        shell = require("shelljs"), 
+        copydir = require("copy-dir"), 
+        beautify = require('beautify'), 
+        beaut = (obj) => beautify(JSON.stringify(obj), { format: 'json' });
+
+    // Gathering layouts
+    const pageLayout = require('./_layouts/page.js'),
+        homeItemModule = require('./_modules/homeItem'),
+        // itemModule = require('./_modules/item'),
+        headModule = require('./_modules/head');
+
+    // Meta Data Gatherer
+    let globals = require('./_config.js'),
+    domain = 'data.compiledmcr.com',
+    site = `https://${domain}`,
+    sitemap = [];
+
     // Moving static files to site
     shell.mkdir('-p', './_site');
     shell.mkdir('-p', './_site/data');
     shell.mkdir('-p', './_site/_data');
     copydir.sync('./_static', './_site');
     copydir.sync('./_data', './_site/_data');
+    
     // Generate _site
+
+    let datasetDesc = (location, locationName) => `If you wish to view this data, you can either visit the Github repo, or download it directly from <a href="${ location }" targer="_blank">${ locationName }</a>.`;
+
+    let datasets = [{
+        name: "Groups",
+        count: numberWithCommas(groups.length),
+        desc: datasetDesc("./_data/groups.json", "here")
+    }, {
+        name: "Events",
+        count: numberWithCommas(events.length),
+        desc: datasetDesc("./_data/events.json", "here")
+    }, {
+        name: "Attendees",
+        count: numberWithCommas(attendees.length),
+        desc: datasetDesc("./_data/attendees.json", "here")
+    }];
+
+    let pages = []; // TODO
+
+    // Creating Home Page
+    let index = pageLayout(
+        headModule(globals.metaTitlePrefix + globals.title, globals.desc, globals.metaTags),
+        globals.img,
+        globals.title,
+        globals.desc,
+        globals.social,
+        globals.itemNames,
+        datasets.map(dataset => {
+            return homeItemModule(
+                `${dataset.name} (${dataset.count})` ,
+                dataset.desc,
+                "",
+                dataset.background ? dataset.background : ""
+            )
+        }).join("\n"),
+        globals.exampleNames,
+        pages.map(page => {
+            return homeItemModule(
+                `${page.name}` ,
+                page.desc,
+                page.slug,
+                page.background ? page.background : ""
+            )
+        }).join("\n"),
+        globals.background
+    );
+
+    fs.writeFileSync(`./_site/${globals.slug}.html`, index, () => {});
 
     // Generate Information from Large Data
     fs.readdirSync(`${__dirname}/_scripts`).forEach(script => {
@@ -14,4 +80,8 @@ module.exports = (groups, events, attendees) => {
         let fileName = script.split("/").slice(-1)[0].replace(".js", ".json");
         fs.writeFileSync(`${__dirname}/_site/data/${fileName}`, beaut(result), () => {});
     });
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
